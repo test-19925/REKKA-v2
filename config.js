fc9a1ef73d4e8f35bae210d5c61393f25ca4866c
test-19925 <reka.suska12334@gmail.com>
2026-07-31 16:25:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 212666666666;
+global.pairingNumber = 201015350801;
 global.owner = [
-  ['212717457920', 'noureddine Ouafy', true],
+  ['201015350801', 'noureddine Ouafy', true],
   ['', 'Owner 2', true],
 ];
 
